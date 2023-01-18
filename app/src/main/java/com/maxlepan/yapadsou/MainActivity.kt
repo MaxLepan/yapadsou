862575f1f31a9b49a26c0ea815a82ecaccc75609
Coyls <loic.grecofr@gmail.com>
2023-01-18 08:47:49 +0100

    refactor(notification): try to setup notification

--- app/src/main/java/com/maxlepan/yapadsou/MainActivity.kt
+++ app/src/main/java/com/maxlepan/yapadsou/MainActivity.kt
@@ -1,22 +1,95 @@
 package com.maxlepan.yapadsou
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 import com.maxlepan.yapadsou.modules.Connection.ConnectionView
 import com.maxlepan.yapadsou.modules.Home.Home
-import com.maxlepan.yapadsou.modules.Onboarding.OnboardingView
 import com.maxlepan.yapadsou.ui.theme.YapadsouTheme
+import java.util.jar.Manifest
 
 class MainActivity : ComponentActivity() {
+
+
+
+
+
+    val CHANNEL_ID = "Yapadsou"
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.app_name)
+            val descriptionText = getString(R.string.default_web_client_id)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = getString(R.string.app_name, token)
+            Log.d(TAG, msg)
+            // Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+
+            val builder = NotificationCompat.Builder(this, msg)
+                .setSmallIcon(androidx.core.R.drawable.notification_action_background)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+
+            with(NotificationManagerCompat.from(this)) {
+               notify(0, builder.build())
+            }
+        })
+
         setContent {
             YapadsouTheme {
                 // A surface container using the 'background' color from the theme
